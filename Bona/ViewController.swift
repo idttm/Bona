771fef085cda8a796fbac11ca5ad4d8fc60757c3
idttm <andrew79911997@gmail.com>
2020-08-04 16:25:38 +0300

    Initial Commit

---
+++ Bona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bona
+//
+//  Created by Andrew Cheberyako on 04.08.2020.
+//  Copyright © 2020 Andrew Cheberyako. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
